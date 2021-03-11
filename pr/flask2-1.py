0c5e0a3e91fef02488129b65f4e42fd6e2f11e81
Alexandr Stepanov <65487681+Xanderstp@users.noreply.github.com>
2021-03-11 16:46:06 +0300

    Add files via upload

---
+++ pr/flask2-1.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from flask import url_for
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='Домашняя страница', 
+                           username=user)
